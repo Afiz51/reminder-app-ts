1c72d127ca013fad0ea921435cd39617c72b2392
Gboyega Lawal <afiz.lawal51@gmail.com>
2022-06-12 00:56:44 +0100

    delete reminder

---
+++ src/components/ReminderList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Reminder from "./types/reminder";
+
+interface ReminderListProps {
+  items: Reminder[];
+  onRemoveReminer: (id: number) => void;
+}
+const ReminderList = (props: ReminderListProps) => {
+  return (
+    <ul className="list-grop">
+      {props.items.map((item) => (
+        <li className="list-group-item" key={item.id}>
+          {item.title}
+          <button
+            onClick={() => props.onRemoveReminer(item.id)}
+            className="btn btn-outline-danger mx-2 rounded-pill"
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ReminderList;
